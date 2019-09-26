8016bbe26347d420bd66a73ef18aab27e56298ce
Stephanie Ramos <stephanien.ramos@Stephanies-MacBook-Pro-2.local>
2019-09-26 10:42:38 -0400

    added types-variables.playground

---
+++ Unit 1/Types-Variables.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+var str = "Hello, playground"
+
+str
+
+//
+var fellowName = "Stephanie Ramos"
+
+fellowName
+
+var cohort: String = "iOS 6.3"
+
+var alphabet: Character = "c"
+
+
+cohort = "7.0"
+
+var age = 32
+
+var ageOfFellow = 32
+var futureAge = ageOfFellow + 8
+
+//String interpolation
+print("Age of fellow 8 years from now is \(futureAge).")
+
+print(1 + 4 * 2 / 2 + 2)
+
+let numOne = 4.0
+let numTwo = 4.0
+let a = numOne == numTwo
+
+var i = 0
+i = 5
+i += 3
+i *= 2
+i %= 3
+i -= 3
+
+
+let comboIT = !true
+print(comboIT)
+false
+
+let d1 = 1.1
+let d2 = 1.1
+
+if d1 == d2 {
+    print("d1 and d2 are equal")
+}
+
+print("d1 + 0.1 is \(d1 + 0.1)")
+
+if d1 + 0.1 == 1.2 {
+    print("hello, out there.")
+} else {
+  print("d1 + 0.1 is not 1.2")
+}
+
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+print(pi)
+
