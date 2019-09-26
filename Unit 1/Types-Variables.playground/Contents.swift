15e54077b9397e2a2422779611be576367de467d
Stephanie Ramos <stephanien.ramos@Stephanies-MacBook-Pro-2.local>
2019-09-26 12:51:42 -0400

    updated file

--- Unit 1/Types-Variables.playground/Contents.swift
+++ Unit 1/Types-Variables.playground/Contents.swift
@@ -23,10 +23,14 @@ var str = "Hello, playground"
 // declared a variable fellowName of type String
 // Type Inference: implicit declaration of a variable I
 var fellowName = "David Lin"
+fellowName = "34"
 
+// Does not compile - cannot add different data types
+// let newString = "34" + 12
 
 // Type Annotation - explicity writing the data type using : (colon)
 var cohort: String = "iOS 6.3"
+
 
 //==============================================
 //                Character
@@ -39,20 +43,20 @@ var alphabet: Character = "c"
 cohort = "iOS 7.3"
 
 
-
 //==============================================
 //                   Int
 //==============================================
 
 // declared variable age and initialized value to 32
-
 var age = 32
 var ageOfFellow = 32
 var futureAge = ageOfFellow + 8
 // String interpolation is embedding a variable into a String using \(variable name) syntax as seen below
 print("Age of fellow 8 years from now is \(futureAge).")
 
-
+// integer division truncates decimals (removes them)
+let integerDivision = 16/3
+print("Interger division is \(integerDivision).")
 
 //==============================================
 //                 Double
@@ -62,82 +66,49 @@ var balance = 100005.9
 
 // re-assign balance a new value of 85000
 balance = balance + 85000
-
 print(balance)
 
+// floating point division keeps decimals
+// let floatingPointDivision: Double  = 16.0/3.0 (also valid to keep decimal points)
+let floatingPointDivision = 16.0/3.0
+print("Floating point division is \(floatingPointDivision).")
 
 //==============================================
-//            Short hand arithmetic
+//                 Float
+//==============================================
+
+// Float holds 32-bit vs. 64-bit of a double (double holds more memory than a float)
+let floatNum:Float = 67.187352930873
+let doubleNum:Double = 67.187352930873
+print("Float is \(floatNum) and double is \(doubleNum).")
+
+// Does not compile - cannot add a float with a double
+// let accountBalance = floatNum + doubleNum
+
+// casting (converting) a float to a double
+let accountBalance = Double(floatNum) + doubleNum
+print("Account balance is now \(accountBalance).")
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pie = Double(three) + pointOneFourOneFiveNine
+print(pie)
+
+//==============================================
+//           Short-hand Arithmetic
 //==============================================
 
 balance -= 180000
-
 print(balance)
-
-
-//==============================================
-//         Boolean - Bool, Comparison Operators
-//==============================================
-
-var result = 70 < 100
-
-print(result)
-
-var otherResult = result && true
-
-print(otherResult)
-
-
--5 > -2 && 3 >= -5
-
-//==============================================
-//          Arithmetic operators
-//==============================================
-
-//==============================================
-// THIS DOES NOT COMPILE - let is immuatable - constant
-//==============================================
-//let salary = 20000
-//salary *= 2
-//print(salary)
 
 var salary = 20000
 salary *= 2
 // this line above is the same as (salary = salary * 2)
 print(salary)
 
-
 var marathonTime = 6 // 6 hours
 marathonTime /= 2
 print(marathonTime) // 3
-
-
-//==============================================
-//   Modulo operator or Remainder operator
-//==============================================
-var modResult = 16 % 3
-print(modResult)
-
-var even = 12 % 6
-print(even)
-
-
-//==============================================
-//                Constants
-//==============================================
-
-let pi = 3.14
-
-
-//==============================================
-//                Bonus
-//==============================================
-
-print(1 + 4 * 2 / 2 + 2)
-
-let numOne = 4.0
-let numTwo = 4.0
-let a = numOne == numTwo
 
 var i = 0
 i = 5
@@ -145,12 +116,44 @@ i += 3
 i *= 2
 i %= 3
 i -= 3
+print(i)
+
+//==============================================
+//          Division by 0
+//==============================================
+
+// 89/0 Does not compile
+
+
+//==============================================
+//    Boolean - Bool, Comparison Operators
+//==============================================
+
+var result = 70 < 100
+print(result)
+
+var otherResult = result && true
+print(otherResult)
+
+
+print(-5 > -2 && 3 >= -5)
 
 let comboIT = !true
 print(comboIT)
-false
 
+let numOne = 4.0
+let numTwo = 4.0
+let a = numOne == numTwo
+print(a)
 
+let lowercaseName = "alex"
+let uppercaseName = "Alex"
+let stringResult = lowercaseName == uppercaseName
+print(stringResult)
+
+// strings with a comparison operator
+let alphabeticalOrder = "Kelyby" < "Lorraine"
+print("alphabeticalOrder result is \(alphabeticalOrder).")
 
 let d1 = 1.1
 let d2 = 1.1
@@ -168,9 +171,35 @@ if d1 + 0.1 == 1.2 {
 }
 
 
-let three = 3
-let pointOneFourOneFiveNine = 0.14159
-let pie = Double(three) + pointOneFourOneFiveNine
-print(pie)
+//==============================================
+//                Constants
+//==============================================
+
+let pi = 3.14
+
+/*let salary = 20000
+salary *= 2
+print(salary)
+THIS DOES NOT COMPILE - let is immuatable - constant
+ */
+
+
+//==============================================
+//   Modulo Operator or Remainder Operator
+//==============================================
+var modResult = 16 % 3
+print(modResult)
+
+
+var even = 12 % 6
+print(even)
+
+//==============================================
+//
+//==============================================
+
+
+
+
 
 
